c6c63b8a1925d718874461e8e598344917963ed2
ade <ade.li.indie@gmail.com>
2017-10-09 17:29:41 +0800

    add travis-ci support

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ROOT_DIR=`pwd`
+BUILD_OUTPUT="${ROOT_DIR}/build/"
+RELEASE_DIR=${ROOT_DIR}/release/
+
+echo ${BUILD_OUTPUT}
+
+autoreconf --install
+mkdir -p ${BUILD_OUTPUT} 
+cd ${BUILD_OUTPUT} 
+../configure
+make
+
+# do release
+mkdir -p ${RELEASE_DIR}/bin
+ln -sf ${BUILD_OUTPUT}/netcode/netcode ${RELEASE_DIR}/bin/netcode
+ln -sf ${ROOT_DIR}/scripts/ ${RELEASE_DIR}/scripts
